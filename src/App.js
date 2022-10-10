e18019fed9c41134525e8a8568637627225daf43
Victor Seidman <victorseidman@gmail.com>
2022-10-10 18:45:25 -0300

    Check issues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar.js";
+import { Banner } from "./components/Banner.js";
+import { Skills } from "./components/Skills.js";
+import { Projects } from "./components/Projects.js";
+import { Contact } from "./components/Contact.js";
+import { Footer } from "./components/Footer.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react"
+import { Container, Row, Col } from "react-bootstrap"
+import { ArrowRightCircle } from "react-bootstrap-icons"
+import hearderImg from "../assets/img/header-img.svg"
+export const Banner = () => {
+    const[loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Front End Developer", "Back End Developer"]
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+
+        return () => {clearInterval(ticker)}
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i]
+        let updateText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updateText);
+
+        if (isDeleting){
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if(!isDeleting && updateText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updateText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my portfolio</span>
+                        <h1>{`Hi im Victor `}<span className="wrap">{text}</span></h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <button onClick={() => console.log('connect')}>Let Connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={hearderImg} alt="Header img"/>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
